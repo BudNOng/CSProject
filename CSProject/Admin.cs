c1f7b7248c535c829485ff434ca075b99b6f8a8c
budong drummond <budong21@hotmail.com>
2020-04-23 21:05:22 +0100

    Admin Class code added

--- CSProject/Admin.cs
+++ CSProject/Admin.cs
@@ -4,5 +4,38 @@ using System.Text;
 
 namespace CSProject
 {
+    class Admin : Staff
+    {
+        private const float _overtimeRate = 15.5f;
+        private const float _adminHourlyRate = 30;
 
+        public float Overtime { get; private set; }
+        public int OvertimeHours { get; private set; }
+
+        public Admin(string name): base(name, _adminHourlyRate)
+        {
+
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if (HoursWorked > 160)
+            {
+                OvertimeHours = HoursWorked - 160;
+                float Overtime = _overtimeRate * (HoursWorked - 160);
+
+                TotalPay = TotalPay + Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nAdmin Staff named " + NameOfStaff + " have worked for " + HoursWorked + " hours." +
+                "\nOn top of that this staff have worked " + OvertimeHours + " overtime hours" +
+                "\n******* Total pay for this Admin Staff is " + TotalPay + " *******";
+
+        }
+    }
 }
--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -6,10 +6,19 @@ namespace CSProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello There!, welcome to the Payroll system");
+            /*Console.WriteLine("Hello There!, welcome to the Payroll system");
             Console.WriteLine("___________________________________________");
             Console.ReadKey();
 
+            var test1 = new Customers("Budong", 1);
+            Console.WriteLine(test1.ToString());
+            test1.orders.Add(new Order());
+            test1.orders.Add(new Order());
+            test1.orders.Add(new Order());
+
+            Console.WriteLine(test1.orders.Count);*/
+            
+            
             //instantiate the object Staff1 from Staff Class.
             var staff1 = new Staff("Budong Drummond", 10);
             staff1.HoursWorked = 10;            
@@ -19,15 +28,25 @@ namespace CSProject
             Console.WriteLine("\n___________________________________________");
             Console.ReadKey();
 
-            //instantiate the object manager1 from Manager Class the child of Staff class.
+            //instantiate the object manager1 from Manager Class - the child of Staff class.
             var manager1 = new Manager("Clare Carney");
             manager1.HoursWorked = 180;
-            Console.WriteLine(manager1.ToString());
             manager1.CalculatePay();
+            Console.WriteLine(manager1.ToString());
+
+            Console.WriteLine("\n___________________________________________");
+            Console.ReadKey();
+
+            //instantiate the object admin1 from Admin Class - the child of Staff class.
+            var admin1 = new Admin("John Legend");
+            admin1.HoursWorked = 170;
+            admin1.CalculatePay();
+            Console.WriteLine(admin1.ToString());
 
             Console.WriteLine("\n___________________________________________");
             Console.WriteLine("\n-----------Click any key to exit-----------");
             Console.ReadKey();
+            
         }
     }
 }
